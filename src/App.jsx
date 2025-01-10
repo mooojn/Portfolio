e2c3ddbada308a7773d2bd4259c6162340563e2c
mooojn <moojntariq@gmail.com>
2025-01-10 22:38:57 +0500

    create files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hi Im Munees Tariq</h1>
+     <h3>This is my portfolio currently under development</h3>
+    </>
+  )
+}
+
+export default App
