7137e6ee37fa2c4d3aeb0b8ca8d4d56a853117cc
mooojn <moojntariq@gmail.com>
2025-01-11 18:42:16 +0500

    Add headlines and ref for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import Header from './components/Header.jsx'
+import './styles/App.css'
 
 function App() {
 
   return (
     <>
-     <h1>Hi Im Munees Tariq</h1>
-     <h3>This is my portfolio currently under development</h3>
+      <Header />
+
+      <div className='headline'>
+        <h1>Helping clients build innovative, efficient solutions fast.</h1>
+        <p>If you're an early-stage founder, SaaS owner, or indie-maker seeking efficient and innovative solutions, I'd love to collaborate.</p>
+        <p>Let's build something amazing together!</p>
+      </div>
     </>
   )
 }
