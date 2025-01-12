c0c8d8a5457575c3e64559fc88cb099b336a1e9b
mooojn <moojntariq@gmail.com>
2025-01-12 17:55:51 +0500

    Add object of PAGEX

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,12 +6,26 @@ import pagexLogo from '../assets/skills/react.svg'
 import '../styles/Project.css'
 
 function Project() {
+    const PAGEX = {
+        image: pagex,
+        logo: pagexLogo,
+        name: "PageX",
+        heading: "Developed text editor with DSA for seamless work experience",
+        highlights: [
+            "React.js text editor with advanced features and rich functionality.",
+            "Supports text formatting and PDF export with seamless integration.",
+            "DJB2 hash function for search, reducing key collisions by 47%.",
+            "Hybrid Kruskal's and Prim's algorithm, cutting maze generation time by 67%.",
+            "DSA concepts enhancing data access and manipulation speed by 39%."
+        ],                   
+        description: "This was very nice",
+    };
 
     return (
         <>
             <div className='section'>
                 <h1 className='legend'>Projects</h1>
-                <ProjectCard image={pagex} logo={pagexLogo} siteName={"PageX"} heading={"Developed text editor with DSA for seamless work experience"} highlights={["speed", "efficient"]} description={"This was very nice"}/>
+                <ProjectCard info={PAGEX} />
             </div>
         </>
     )
