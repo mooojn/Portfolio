c0f7a820c21833202f9ea8c177931a1a80730670
mooojn <moojntariq@gmail.com>
2025-01-12 15:37:59 +0500

    Add project component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Headline from './components/Headline.jsx'
 import Skill from './components/Skill.jsx'
+import Project from './components/Project.jsx'
 import './styles/App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Headline />
       <Skill />
+      <Project />
     </>
   )
 }
