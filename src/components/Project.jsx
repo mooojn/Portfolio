6c7600cedbfb9c0af5167a2d5d08d01f249c7286
mooojn <moojntariq@gmail.com>
2025-01-12 15:38:11 +0500

    Create Project.jsx

---
+++ src/components/Project.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import '../styles/Project.css'
+
+function Project() {
+
+    return (
+        <>
+            <div className='section'>
+                <h1 className='legend'>Projects</h1>
+            </div>
+        </>
+    )
+}
+
+export default Project
