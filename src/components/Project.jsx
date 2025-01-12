1c3d9c4471330ba471cb8b735fb81c3974e2562e
mooojn <moojntariq@gmail.com>
2025-01-12 16:48:36 +0500

    Add project card

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import ProjectCard from './ProjectCard.jsx'
 
+import pagex from '../assets/projects/page-x.png'
+import pagexLogo from '../assets/skills/react.svg'
 import '../styles/Project.css'
 
 function Project() {
@@ -8,6 +11,7 @@ function Project() {
         <>
             <div className='section'>
                 <h1 className='legend'>Projects</h1>
+                <ProjectCard image={pagex} logo={pagexLogo} siteName={"PageX"} heading={"Developed text editor with DSA for seamless work experience"} highlights={["speed", "efficient"]} description={"This was very nice"}/>
             </div>
         </>
     )
